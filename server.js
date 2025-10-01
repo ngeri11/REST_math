516ebd526b891e6020b1265fa9039ffabd9e9773
ImreS234 <szegediimremark@gmail.com>
2025-10-01 10:26:51 +0200

    Math Backend kész

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+//POST a végpont a klienstől érkező adatok fogadására.
+app.post('/api/math', (req, res) =>{
+    //A klienstől érkező adatok elérése változókban
+    const {num1, num2, operator} =req.body; // Objektum description
+
+    //Backend oldali validáció - Ellenőrizzük, hogy mind a két szám létezik és szám típúsú
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return res.status(400).json ({error: 'Hiányzó, vagy érvénytelen számok'});
+    }
+
+    //Számolási logika
+    let result;
+    if(operator == "add" ){
+        result = num1 + num2;
+
+    }
+    else if (operator == "subtract"){
+        result = num1 -num2;
+    }
+    else{
+        return res.status(400).json ({message:'Érvénytelen művelet'});
+    }
+    res.status(200).json({message: 'A kiszámolt eredmény:' ,result});
+})
+
+app.listen(port, () =>{
+    console.log(`A webszerver fut a http://localhost:${port} webcímen`);
+});
